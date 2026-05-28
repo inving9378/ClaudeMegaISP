09954fa415a55ae1ad2dafa2345b39bb156b6ab7
Carlos <carlos.rodriguez@motorflash.com>
2026-05-28 18:28:42 -0400

    compilando y ajustando

--- database/migrations/2026_05_07_075605_change_field_types_for_tasks.php
+++ database/migrations/2026_05_07_075605_change_field_types_for_tasks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Module;
-use App\Models\FieldType;
+use App\Modules\Core\Configuracion\Models\FieldType;
 
 return new class extends Migration
 {
--- database/migrations/2026_05_15_074848_update_ticket_details_field_types.php
+++ database/migrations/2026_05_15_074848_update_ticket_details_field_types.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\Module;
-use App\Models\FieldType;
+use App\Modules\Core\Configuracion\Models\FieldType;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
