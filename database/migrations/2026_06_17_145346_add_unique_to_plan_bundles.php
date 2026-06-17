c54fe3e2b2be73306d0030178501b9d5d7d13859
Carlos <carlos.rodriguez@motorflash.com>
2026-06-17 19:21:25 -0400

    ajustando

--- database/migrations/2026_06_17_145346_add_unique_to_plan_bundles.php
+++ database/migrations/2026_06_17_145346_add_unique_to_plan_bundles.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,18 +12,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('plan_bundles', function (Blueprint $table) {
-            $table->unique(
-                ['bundle_id', 'plan_bundle_id', 'plan_bundle_type'],
-                'plan_bundles_bundle_plan_type_unique'
-            );
-        });
+//        Schema::table('plan_bundles', function (Blueprint $table) {
+//            $table->unique(
+//                ['bundle_id', 'plan_bundle_id', 'plan_bundle_type'],
+//                'plan_bundles_bundle_plan_type_unique'
+//            );
+//        });
     }
 
     public function down(): void
     {
-        Schema::table('plan_bundles', function (Blueprint $table) {
-            $table->dropUnique('plan_bundles_bundle_plan_type_unique');
-        });
+//        Schema::table('plan_bundles', function (Blueprint $table) {
+//            $table->dropUnique('plan_bundles_bundle_plan_type_unique');
+//        });
     }
 };
