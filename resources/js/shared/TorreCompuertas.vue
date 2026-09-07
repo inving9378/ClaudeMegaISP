fb6ff0c5f5dc4916cad579e69718dbafe1ac9462
Irving MegaISP <tu@email.com>
2026-09-07 10:01:29 -0600

    Agrega clases hdr-badge/hdr-badge-corner al badge del engrane (#9990514)

--- resources/js/shared/TorreCompuertas.vue
+++ resources/js/shared/TorreCompuertas.vue
@@ -16,7 +16,7 @@
         <a class="btn header-item position-relative" :href="destino" :title="titulo" data-spa-skip>
             <i class="fas fa-cog" :class="{ 'text-danger': hayBloqueo, 'fa-spin': cargando }"></i>
             <span v-if="hayBloqueo"
-                  class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger hdr-badge hdr-badge-corner">
                 {{ rojos }}
             </span>
         </a>
