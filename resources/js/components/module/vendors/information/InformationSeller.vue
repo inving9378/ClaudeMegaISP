5237c926848dda018d8df465eb8d0d357c7e798c
Irving MegaISP <tu@email.com>
2026-09-08 06:07:50 -0600

    Ficha vendedor: agrega mt-4 al card Documentos para separarlo (item #9990598)

--- resources/js/components/module/vendors/information/InformationSeller.vue
+++ resources/js/components/module/vendors/information/InformationSeller.vue
@@ -205,7 +205,7 @@
 
     <div class="row" v-if="activeTab === '#navs-pills-justified-information' && hasDocumentPermission">
         <div class="col-md-12">
-            <div class="card mb-4">
+            <div class="card mb-4 mt-4">
                 <h5 class="card-header">Documentos</h5>
                 <div class="card-body">
                     <div v-if="!colaboradorId" class="text-muted small text-center py-3">
